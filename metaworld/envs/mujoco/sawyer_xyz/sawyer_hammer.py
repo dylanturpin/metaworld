723ffc3f7ecf60792dbfda7942cd0452a4f31468
Dylan Turpin <dylanturpin@Dylans-MacBook-Pro.local>
2020-05-10 15:09:52 -0400

    enable done signal

--- metaworld/envs/mujoco/sawyer_xyz/sawyer_hammer.py
+++ metaworld/envs/mujoco/sawyer_xyz/sawyer_hammer.py
@@ -49,7 +49,7 @@ class SawyerHammerEnv(SawyerXYZEnv):
 
         if goal_low is None:
             goal_low = self.hand_low
-        
+
         if goal_high is None:
             goal_high = self.hand_high
 
@@ -133,7 +133,8 @@ class SawyerHammerEnv(SawyerXYZEnv):
         #                             'hammerRew': hammerRew, 'epRew' : reward, 'hammerDist': hammerDist, 'screwDist': screwDist}
         info = {'reachDist': reachDist, 'pickRew':pickRew, 'epRew' : reward, 'goalDist': screwDist, 'success': float(screwDist <= 0.05)}
         info['goal'] = self.goal
-        return ob, reward, False, info
+        done = (self.curr_path_length == self.max_path_length)
+        return ob, reward, done, info
 
     def _get_obs(self):
         hand = self.get_endeff_pos()
@@ -180,7 +181,7 @@ class SawyerHammerEnv(SawyerXYZEnv):
         self.data.site_xpos[self.model.site_name2id('objSite')] = (
             objPos
         )
-    
+
 
 
 
@@ -260,7 +261,7 @@ class SawyerHammerEnv(SawyerXYZEnv):
         return np.array(rewards)
 
     def compute_reward(self, actions, obs, mode='orig'):
-        if isinstance(obs, dict): 
+        if isinstance(obs, dict):
             obs = obs['state_observation']
 
         hammerPos = obs[3:6]
@@ -299,15 +300,15 @@ class SawyerHammerEnv(SawyerXYZEnv):
 
 
         def objDropped():
-            return (hammerPos[2] < (self.hammerHeight + 0.005)) and (hammerDist >0.02) and (reachDist > 0.02) 
+            return (hammerPos[2] < (self.hammerHeight + 0.005)) and (hammerDist >0.02) and (reachDist > 0.02)
             # Object on the ground, far away from the goal, and from the gripper
             #Can tweak the margin limits
-       
+
         def objGrasped(thresh = 0):
             sensorData = self.data.sensordata
             return (sensorData[0]>thresh) and (sensorData[1]> thresh)
 
-        def orig_pickReward():       
+        def orig_pickReward():
             # hScale = 50
             hScale = 100
             if self.pickCompleted and not(objDropped()):
